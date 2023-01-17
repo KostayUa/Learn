1b6b7e52330c309dd392a5201b4e6e33fc6a2b50
KostayUa <kostay.ua@gmail.com>
2023-01-17 14:46:01 +0200

    Added the game.

---
+++ src/main/java/Game6.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Game6 {
+    public static void main(String[] args) throws IOException {
+
+        char ch, answer = 'B';
+
+        System.out.println("Какую букву я загадал ? ");
+        System.out.print("Попытайтесь ее угадать: ");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer) {
+            System.out.print("Поздровляем! ");
+        } else if (ch > answer) {
+            System.out.print("Перестарался ! ");
+        } else {
+            System.out.print("Недостарался !");
+        }
+    }
+}
