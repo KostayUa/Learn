fc8488dcd2e47db67e42f5eb43f0b9874d814d9a
KostayUa <kostay.ua@gmail.com>
2023-01-17 14:41:11 +0200

    Added the lesson 7_2.

---
+++ src/main/java/Start7_2.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Start7_2 {
+
+    public static void main(String[] args) throws IOException {
+        int count = 0;
+            while (count <10){
+                System.out.println(count);
+                count++;
+            }
+    }
+}
