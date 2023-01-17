dde64dbaa78a54fe837d480162cbf370d93c7703
KostayUa <kostay.ua@gmail.com>
2023-01-17 14:43:20 +0200

    Added the lesson 7_4.

---
+++ src/main/java/Start7_4.java
@@ -0,0 +1,11 @@
+public class Start7_4 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            count++;
+
+            if ((count % 2) != 0) continue;
+            System.out.println(count);
+        }
+    }
+}
