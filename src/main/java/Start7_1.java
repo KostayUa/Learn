2be88b7c3bcb754372d8e8e3a77fd6b19c9f17a1
KostayUa <kostay.ua@gmail.com>
2023-01-17 14:40:21 +0200

    Added the lesson 7_1.

---
+++ src/main/java/Start7_1.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Start7_1 {
+
+        public static void main(String[] args) throws IOException {
+            for (int i = 0;(char) System.in.read() !='s'; i++) {
+                System.out.println("Итерация" + i);
+            }
+        }
+}
