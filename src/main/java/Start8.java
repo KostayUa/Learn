ea29eaf0499af032c66cecb25dc2f1c80ce67105
KostayUa <kostay.ua@gmail.com>
2023-01-19 20:31:46 +0200

    Added the game.

---
+++ src/main/java/Start8.java
@@ -0,0 +1,23 @@
+public class Start8 {
+    public static void main(String[] args) {
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++) {
+            arr[i] = i;
+        }
+
+        arr[3] = 100;
+
+        //for (int i = 0; i < 4; i++) {
+        for (int i : arr) {
+           // System.out.println("Элкмент[" + i + "]" + arr[i]);
+            System.out.println(i);
+        }
+    }
+}
+
+
+/*Многомерные массивы (пример двумерного массива)
+        int arr[][] = new arr[3][];
+        arr[0] = new int[4];
+        arr[1] = new int[4];
+        arr[2] = new int[40];*/
