fb6380e847eba68dace7c6cd9ad7358977844f74
KostayUa <kostay.ua@gmail.com>
2023-01-24 17:35:17 +0200

    Added the Work Class.

---
+++ src/main/java/Computer.java
@@ -0,0 +1,24 @@
+public class Computer {
+    private int memory;
+    private int cpu;
+    private String name;
+
+    Computer() {};
+    Computer(int memory, int cpu, String name) {
+        this.memory = memory;
+        this.cpu = cpu;
+        this.name = name;
+    }
+
+    void getCpu() {
+        System.out.println(cpu);
+    }
+
+    void getMemory() {
+        System.out.println(memory);
+    }
+
+    void setMemory(int m) {
+        memory = m;
+    }
+}
---
+++ src/main/java/Computer1_1.java
@@ -0,0 +1,8 @@
+public class Computer1_1 {
+    public static void main(String[] args) {
+        Computer apple = new Computer();
+
+        apple.getMemory();
+    }
+
+}
